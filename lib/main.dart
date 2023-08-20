14dd283614c00d2bac4989747ad4a46d49c21329
diki816 <dikisun@gmail.com>
2023-08-20 14:55:32 +0900

    Sign Up Completed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -15,9 +16,8 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
         //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
