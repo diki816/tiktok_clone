130da36495f259bf36e4585ca91de08a0c765f6b
diki816 <dikisun@gmail.com>
2023-10-22 23:31:07 +0900

    4.6, 4.7 password Screen Completed!

--- lib/features/authentication/widgets/email_screen.dart
+++ lib/features/authentication/widgets/email_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/authentication/widgets/password_screen.dart';
 
 class EmailScreen extends StatefulWidget {
   const EmailScreen({super.key});
@@ -33,66 +34,94 @@ class _EmailScreenState extends State<EmailScreen> {
     super.dispose();
   }
 
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+    if (!regExp.hasMatch(_email)) {
+      return "Email not valid";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        // main.dart appBarTheme로 설정
-        // foregroundColor: Colors.black,
-        // backgroundColor: Colors.white,
-        // elevation: 0,
-        title: const Text(
-          "Sign up",
-          // style: TextStyle(
-          //   fontSize: Sizes.size20,
-          //   fontWeight: FontWeight.w600,
-          // ),
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          // main.dart appBarTheme로 설정
+          // foregroundColor: Colors.black,
+          // backgroundColor: Colors.white,
+          // elevation: 0,
+          title: const Text(
+            "Sign up",
+            // style: TextStyle(
+            //   fontSize: Sizes.size20,
+            //   fontWeight: FontWeight.w600,
+            // ),
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            const Text(
-              "What is your email?",
-              style: TextStyle(
-                fontSize: Sizes.size24,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            Gaps.v16,
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: "Email",
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade100,
-                  ),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                "What is your email?",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              cursorColor: Theme.of(context).primaryColor,
-            ),
-            Gaps.v16,
-            FormButton(disabled: _email.isEmpty),
+              Gaps.v16,
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                onEditingComplete: _onSubmit,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  errorText: _isEmailValid(),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade100,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v16,
+              FormButton(disabled: _email.isEmpty || _isEmailValid() != null),
 
-            // TextButton(
-            //   onPressed: () {},
-            //   child: const Text('Next'),
-            // )
-          ],
+              // TextButton(
+              //   onPressed: () {},
+              //   child: const Text('Next'),
+              // )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_email.isEmpty || _isEmailValid() != null) return;
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const PasswordScreen()));
   }
 }
